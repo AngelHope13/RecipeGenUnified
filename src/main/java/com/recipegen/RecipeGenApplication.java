9b57f1aa01744b4b3a67bdd7e7f13f105b9f2c0d
AICA <you@example.com>
2025-06-15 19:11:01 +0800

    Updated application and IntelliJ project settings on partner's machine

--- src/main/java/com/recipegen/RecipeGenApplication.java
+++ src/main/java/com/recipegen/RecipeGenApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RecipeGenApplication {
+public class tRecipeGenApplication {
     public static void main(String[] args) {
         SpringApplication.run(RecipeGenApplication.class, args);
     }
